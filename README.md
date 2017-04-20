a2d4bf6d7f1d324b2a599439f7659dc30bb66ee3
yang.jianan <364804051@qq.com>
2017-04-20 17:44:16 +0800

    输入法布局更新

---
+++ README.md
@@ -0,0 +1,5 @@
+## 自定义输入法
+![](/gif/简单输入法.png)
+![](/gif/新布局.png)
+
+参考文章： http://www.cnblogs.com/palance/p/5120048.html
--- app/src/main/java/com/example/administrator/imm/AndroidInputMethodService.java
+++ app/src/main/java/com/example/administrator/imm/AndroidInputMethodService.java
@@ -1,12 +1,14 @@
 package com.example.administrator.imm;
 
 import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputMethodSubtype;
-import android.widget.Button;
 
 /**
  * Created by yang.jianan on 2017/04/19 14:37.
@@ -14,8 +16,11 @@ import android.widget.Button;
  * 输入法服务的生命周期图：http://img.blog.csdn.net/20130707203833640
  */
 
-public class AndroidInputMethodService extends InputMethodService implements View.OnClickListener {
+public class AndroidInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
     private String TAG = AndroidInputMethodService.class.getName();
+
+    private KeyboardView keyboardView; // 对应keyboard.xml中定义的KeyboardView
+    private Keyboard keyboard; // 对应qwerty.xml中定义的Keyboard
 
     @Override
     public void onCreate() {
@@ -25,20 +30,17 @@ public class AndroidInputMethodService extends InputMethodService implements Vie
 
     @Override
     public View onCreateInputView() {
-        //加载keyboard.xml文件
-        View view = getLayoutInflater().inflate(R.layout.keyboard, null);
-        //键盘 点击事件
-        view.findViewById(R.id.btn1).setOnClickListener(this);
-        view.findViewById(R.id.btn2).setOnClickListener(this);
-        view.findViewById(R.id.btn3).setOnClickListener(this);
-        view.findViewById(R.id.btn4).setOnClickListener(this);
-        view.findViewById(R.id.btn_hide).setOnClickListener(this);
-        Log.d(TAG, "onCreateInputView()");
+        // keyboard被创建后，将调用onCreateInputView函数
+        keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);  // 此处使用了keyboard.xml
+        keyboard = new Keyboard(this, R.xml.qwerty); // 此处使用了qwerty.xml
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
 
-        return view;
+        Log.d(TAG, "onCreateInputView()");
+        return keyboardView;
     }
 
-    @Override
+    /*    @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_hide) {
             // 隐藏软键盘
@@ -57,7 +59,7 @@ public class AndroidInputMethodService extends InputMethodService implements Vie
                 inputConnection.commitText(button.getText(), 1);
             }
         }
-    }
+    }*/
 
     @Override
     public View onCreateCandidatesView() {
@@ -87,5 +89,58 @@ public class AndroidInputMethodService extends InputMethodService implements Vie
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy()");
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE: //删除键
+                inputConnection.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_DONE: //完成键
+                inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                //hideWindow(); //隐藏键盘
+                break;
+            default: //普通输入
+                char code = (char) primaryCode;
+                inputConnection.commitText(String.valueOf(code) + "嘿", 1); //可以对输入的字符串做 加密等等处理
+        }
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
     }
 }
